d026f4cbcce17e90036c11186be23a3c333c4ff2
Mickey <mickeyc11111@gmail.com>
2022-09-05 10:24:02 -0400

    commit tree 68bf41f5182bed83875220ebbbc753b9f1067ed5

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,18 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        array[0]
+    elsif array.length == 2
+        array.join(" and ")
+    elsif array.length >= 3
+        last_item = "and #{array[-1]}"
+        array.pop
+        array << last_item
+        array.join(", ")
+    # elsif array.length > 3
+    #     last_item = "and #{array[-1]}"
+    #     array.pop
+    #     array << last_item
+    #     array.join(", ")
+    end
+   
 end
